fb3177928b1a75f9ca72d22acd0b9696a1312ca2
Ivan Nemenchinskii <agrocooker@gmail.com>
2015-10-17 11:12:50 +0300

    hh

--- FlaskWebProject/test.py
+++ FlaskWebProject/test.py
@@ -51,8 +51,9 @@ result += stdout.strip()+"\n"+string+"\n\n"
 print result
 
 
-#    string = os.path.join(*chunks_cmd)+" "+os.path.join(*chunks_java)+" -jar "+os.path.join(*chunks_jar)
-#    process = Popen(string, shell=True, stdout=PIPE, stderr=PIPE)
-#    stdout, stderr = process.communicate()
-#    result += stdout.strip()+"\n"+string+"\n\n"
+string = os.path.join(*chunks_cmd)+" "+os.path.join(*chunks_java)+" -jar "+os.path.join(*chunks_jar)
+process = Popen(string, shell=True, stdout=PIPE, stderr=PIPE)
+stdout, stderr = process.communicate()
+result += stdout.strip()+"\n"+string+"\n\n"
+print result
 
