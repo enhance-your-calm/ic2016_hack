3723e92e2469defbea556bc3f84be526bd380c39
Ivan Nemenchinskii <agrocooker@gmail.com>
2015-10-17 09:11:55 +0300

    running JAVA from python under cgi

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -1,14 +1,13 @@
 from FlaskWebProject import app
 from flask import render_template
+from subprocess import Popen, PIPE, STDOUT
+string = "java -jar ~/dev/ic2016_hack/FlaskWebProject/hello.jar"
 
 @app.route("/")
 @app.route("/index")
 def index():
-    from FlaskWebProject import test
-
-
-    #return "Hello world!"
-    return render_template(
-            "index.html",
-            test_line = test.stdout
-            )
+    process = Popen(string, shell=True, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    return stdout.strip()
+    print stdout
+    #return stdout
