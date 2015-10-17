7390de4a07de00215e6b5530c3bbd75082dd0a89
Ivan Nemenchinskii <agrocooker@gmail.com>
2015-10-17 12:44:32 +0300

    zav

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -1,6 +1,7 @@
 from FlaskWebProject import app
-from subprocess import Popen, PIPE, STDOUT
+import subprocess
 from flask import render_template, jsonify, request
+import os
 
 
 @app.route("/")
@@ -10,16 +11,28 @@ def index():
 
 @app.route("/resp")
 def resp():
-    tag = request.args.get("tag")
-    start_date = request.args.get("start_date")
+    tag = "'"+request.args.get("tag")+"'"
+    start_date = "'"+request.args.get("start_date")+"'"
+
+
     result = tag + "\n"
     result += start_date + "\n"
+    result += os.getcwd() + "\n"
 
-    parser_machine = Popen(["java", "-jar", "parse.jar", tag, start_date],
-            stdout=PIPE, stderr=PIPE, shell=True)
-    stdout = parser_machine.communicate()[0]
-    stdout = stdout.decode().strip()
-    result+=stdout
+    jar_path = os.getcwd()+"/FlaskWebProject/parser.jar"
+    result += jar_path + "\n"
+    cmd = "java -jar {j} {t} {d}".format(j=jar_path, t=tag, d=start_date)
+    result += cmd + "\n"
+    cmd = ["java","-jar",jar_path,tag,start_date]
+    #parser_machine = Popen(["java", "-jar", "parse.jar", tag, start_date],            stdout=PIPE, stderr=PIPE, shell=True)
+    #parser_machine = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
+    #stdout = parser_machine.communicate()[0]
+    try:
+        stdout = subprocess.check_output(cmd)
+        stdout = stdout.decode().strip()
+        result+=stdout
+    except subprocess.CalledProcessError:
+        result += "ERROR"
     return result
     
     #return "Hello world!"
