ae243a1f238019e1a2d6ace8e6731dae3ffc5b1c
Ivan Nemenchinskii <agrocooker@gmail.com>
2015-10-17 09:21:26 +0300

    pwd

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -1,7 +1,8 @@
 from FlaskWebProject import app
 from flask import render_template
 from subprocess import Popen, PIPE, STDOUT
-string = "java -jar FlaskWebProject\hello.jar"
+#string = "java -jar FlaskWebProject\hello.jar"
+string = "pwd"
 
 @app.route("/")
 @app.route("/index")
